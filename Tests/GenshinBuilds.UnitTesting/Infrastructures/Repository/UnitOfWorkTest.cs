1af7be6b84becfa21181396959c1ffe29515a4ec
Denis-A1zek <denissigida.work@gmail.com>
2023-07-07 19:43:22 +0300

    unit of work and update handlers test

---
+++ Tests/GenshinBuilds.UnitTesting/Infrastructures/Repository/UnitOfWorkTest.cs
@@ -0,0 +1,36 @@
+﻿using GenshinBuilds.Application.Common.Builders;
+using GenshinBuilds.Application;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenshinBuilds.RelationalDb;
+using GenshinBuilds.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using FluentAssertions;
+
+namespace GenshinBuilds.UnitTesting.Infrastructures.Repository;
+
+public class UnitOfWorkTest
+{
+    DatabaseContext dbContextMock;
+    IUnitOfWork unitOfWork;
+
+    [SetUp]
+    public void Setup()
+    {
+        dbContextMock = FixtureFactory.CreateContext();
+        unitOfWork = new UnitOfWork(dbContextMock);
+    }
+
+    [Test]
+    public void GetRepositoryGeneric_ShouldReturn_WeaponRepository()
+    {
+        var repository = unitOfWork.GetRepository<Weapon>();
+
+        repository.Should().NotBeNull();
+    }
+
+}
