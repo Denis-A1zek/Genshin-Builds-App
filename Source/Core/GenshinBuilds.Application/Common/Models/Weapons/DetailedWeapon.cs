f7a592851e485204322845d33f5b8bea96e9069c
Denis-A1zek <denissigida.work@gmail.com>
2023-07-13 12:09:09 +0300

    added app model and interfaces

---
+++ Source/Core/GenshinBuilds.Application/Common/Models/Weapons/DetailedWeapon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenshinBuilds.Application.Common.Models
+{
+    public class DetailedWeapon
+    {
+    }
+}
---
+++ Source/Core/GenshinBuilds.Application/Common/Models/Weapons/MinimalWeapon.cs
@@ -0,0 +1,20 @@
+﻿using GenshinBuilds.Application.Common.Interfaces;
+using GenshinBuilds.Domain;
+using GenshinBuilds.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenshinBuilds.Application.Common.Models;
+
+public class MinimalWeapon : IRare, IContainWeaponType
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public Rarity Rarity { get; set;}
+    public string Image { get; set; }
+    public WeaponType WeaponType { get; set; }
+}
