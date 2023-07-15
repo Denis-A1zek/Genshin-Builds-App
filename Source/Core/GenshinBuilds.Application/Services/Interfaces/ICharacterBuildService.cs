b5f41e5a66571ce10944033985d36376558e8fd1
Denis-A1zek <denissigida.work@gmail.com>
2023-07-15 19:40:37 +0300

    created character build service

---
+++ Source/Core/GenshinBuilds.Application/Services/Interfaces/ICharacterBuildService.cs
@@ -0,0 +1,15 @@
+﻿using GenshinBuilds.Application.Common.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenshinBuilds.Application.Services.Interfaces;
+
+public interface ICharacterBuildService
+{
+    Task InsertBuild(CharacterBuildDto characterBuild);
+    Task ChangeBuildPriority(Guid characterBuildId, int newPriority);
+    Task DeleteBuildPriority(Guid characterBuildId);
+}
